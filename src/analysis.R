993c2779bc37dbab18218934df47b2d236daaf45
Eric Minikel <ericminikel@gmail.com>
2015-11-11 22:55:42 -0500

    comparing to PrP-FEHTA data

--- src/analysis.R
+++ src/analysis.R
@@ -59,9 +59,11 @@ luciferase_inhibitors = read_pubchem('data/AID_588451_datatable_all.csv',skip=6)
 # GFP counterscreen - inactives should be removed from hits
 gfp_counterscreen = read_pubchem('data/AID_588507_datatable_all.csv',skip=6)
 
-
 # PrP-FEHTA primary screen data
 fehta_primary = read_pubchem('data/AID_720596_datatable_all.csv',skip=5)
+
+# PrP-FEHTA confirmaton screen data
+fehta_conf = read_pubchem('data/AID_743200_datatable_all.csv',skip=5)
 
 # generate list of all pubchem CIDs to go get SMILES for them and later compute fingerprints
 cids = unique(c(prnp_5utr_primary$pubchem_cid, snca_5utr_primary$pubchem_cid, fehta_primary$pubchem_cid))
@@ -81,53 +83,66 @@ compounds$prnp_z = pmax(pmin(scale(prnp_5utr_primary$mean[match(compounds$pubche
 snca_5utr_primary$mean = (snca_5utr_primary$replicate_a_activity_score + snca_5utr_primary$replicate_b_activity_score)/2
 compounds$snca_z = pmax(pmin(scale(snca_5utr_primary$mean[match(compounds$pubchem_cid,snca_5utr_primary$pubchem_cid)]),5),-5)
 compounds$app_z = pmax(pmin(scale(app_5utr_primary$ps_inh_2um[match(compounds$pubchem_cid, app_5utr_primary$pubchem_cid)]),5),-5)
+compounds$fehta_z = pmax(pmin(scale(fehta_primary$inhibition.at.13.8.um[match(compounds$pubchem_cid, fehta_primary$pubchem_cid)]),5),-5)
 
 plot(compounds$prnp_z, compounds$snca_z, xlim=c(-5,5), ylim=c(-5,5), xaxs='i', yaxs='i', axes=FALSE, xlab='', ylab='', pch=20, cex=.2)
 abline(h=0, col=axis_col, lwd=2)
 abline(v=0, col=axis_col, lwd=2)
-mtext(side=2, at=0, las=2, line=0, text=expression(italic('PRNP')~'inactive'), col=axis_col)
 mtext(side=4, at=0, las=2, line=0, text=expression(italic('PRNP')~'active'), col=axis_col)
-mtext(side=1, at=0, line=0, text=expression(italic('SNCA')~'inactive'), col=axis_col)
 mtext(side=3, at=0, line=0, text=expression(italic('SNCA')~'active'), col=axis_col)
 rect(xleft=2.5, xright=5, ybottom=-1, ytop=1, border=axis_col, lwd=4)
 
 plot(compounds$prnp_z, compounds$app_z, xlim=c(-5,5), ylim=c(-5,5), xaxs='i', yaxs='i', axes=FALSE, xlab='', ylab='', pch=20, cex=.2)
 abline(h=0, col=axis_col, lwd=2)
 abline(v=0, col=axis_col, lwd=2)
-mtext(side=2, at=0, las=2, line=0, text=expression(italic('PRNP')~'inactive'), col=axis_col)
 mtext(side=4, at=0, las=2, line=0, text=expression(italic('PRNP')~'active'), col=axis_col)
-mtext(side=1, at=0, line=0, text=expression(italic('APP')~'inactive'), col=axis_col)
 mtext(side=3, at=0, line=0, text=expression(italic('APP')~'active'), col=axis_col)
 rect(xleft=2.5, xright=5, ybottom=-1, ytop=1, border=axis_col, lwd=4)
 
+
+
+plot(compounds$prnp_z, compounds$fehta_z, xlim=c(-5,5), ylim=c(-5,5), xaxs='i', yaxs='i', axes=FALSE, xlab='', ylab='', pch=20, cex=.2)
+abline(h=0, col=axis_col, lwd=2)
+abline(v=0, col=axis_col, lwd=2)
+mtext(side=4, at=0, las=2, line=0, text=expression(italic("5'UTR")~'active'), col=axis_col)
+mtext(side=3, at=0, line=0, text=expression(italic('FRET')~'active'), col=axis_col)
+
+
 mean(!is.na(compounds$snca_z[!is.na(compounds$prnp_z)]))
 mean(!is.na(compounds$app_z[!is.na(compounds$prnp_z)]))
+mean(!is.na(compounds$fehta_z[!is.na(compounds$prnp_z)]))
 
 # Table counting hits and how they are progressively eliminated
 hit_table = data.frame(step=character(0), count=integer(0))
-prnp = prnp_5utr_primary[!duplicated(cid_smiles$smiles[match(prnp$pubchem_cid,cid_smiles$pubchem_cid)]),]
-prnp$smiles_avail = prnp$pubchem_cid %in% cid_smiles$cid[!is.na(cid_smiles$smiles)] # all TRUE
 hit_table = rbind(hit_table,cbind('Unique compounds tested',nrow(prnp)))
-prnp$hit = prnp$pubchem_cid %in% plot_data$pubchem_cid[-plot_data$prnp_mean < -50 & abs(plot_data$snca_mean < 25)]
-hit_table = rbind(hit_table,cbind('Hits - PRNP active and SNCA inactive',sum(prnp$hit)))
-prnp$hit = prnp$hit & prnp$reproducibility_cosine_transform > .95 
-hit_table = rbind(hit_table,cbind('Inter-replicate reproducibility > 0.95',sum(prnp$hit)))
-prnp$hit = prnp$hit & !(prnp$pubchem_cid %in% app_counterscreen$pubchem_cid[app_counterscreen$pubchem_activity_outcome=='Active'])
-hit_table = rbind(hit_table,cbind('Inactive in APP counterscreen',sum(prnp$hit)))
-prnp$hit = prnp$hit & !(prnp$pubchem_cid %in% gfp_counterscreen$pubchem_cid[gfp_counterscreen$pubchem_activity_outcome=='Inactive'])
-hit_table = rbind(hit_table,cbind('Active in GFP confirmatory screen',sum(prnp$hit)))
-prnp$hit = prnp$hit & !(prnp$pubchem_cid %in% luciferase_inhibitors$pubchem_cid[luciferase_inhibitors$pubchem_activity_outcome=='Active'])
-hit_table = rbind(hit_table,cbind('Not luciferase inhibitors',sum(prnp$hit)))
-prnp$hit = prnp$hit & !(prnp$pubchem_cid %in% prnp_dose_1$pubchem_cid[prnp_dose_1$pubchem_activity_outcome=='Inactive'])
-prnp$hit = prnp$hit & !(prnp$pubchem_cid %in% prnp_dose_2$pubchem_cid[prnp_dose_2$pubchem_activity_outcome=='Inactive'])
-hit_table = rbind(hit_table,cbind('Not found inactive in PRNP dose-response',sum(prnp$hit)))
-prnp$hit = prnp$hit & !(prnp$pubchem_cid %in% prnp_cherrypick$pubchem_cid[prnp_cherrypick$pubchem_activity_outcome=='Inactive'])
-hit_table = rbind(hit_table,cbind('Not found inactive in cherry pick re-testing',sum(prnp$hit)))
+compounds$prnp_hit = !is.na(compounds$prnp_z) & compounds$prnp_z > 2.5 & !is.na(compounds$snca_z) & abs(compounds$snca_z) < 2 & !is.na(compounds$app_z) & abs(compounds$app_z) < 2
+hit_table = rbind(hit_table,cbind('Hits - PRNP active but SNCA and APP inactive',sum(compounds$prnp_hit)))
+compounds$prnp_hit = compounds$prnp_hit & compounds$pubchem_cid %in% prnp_5utr_primary$pubchem_cid[prnp_5utr_primary$reproducibility_cosine_transform > .95]
+hit_table = rbind(hit_table,cbind('Inter-replicate reproducibility > 0.95',sum(compounds$prnp_hit)))
+compounds$prnp_hit = compounds$prnp_hit & !(compounds$pubchem_cid %in% app_counterscreen$pubchem_cid[app_counterscreen$pubchem_activity_outcome=='Active'])
+hit_table = rbind(hit_table,cbind('Inactive in APP counterscreen',sum(compounds$prnp_hit)))
+compounds$prnp_hit = compounds$prnp_hit & !(compounds$pubchem_cid %in% gfp_counterscreen$pubchem_cid[gfp_counterscreen$pubchem_activity_outcome=='Inactive'])
+hit_table = rbind(hit_table,cbind('Active in GFP confirmatory screen',sum(compounds$prnp_hit)))
+compounds$prnp_hit = compounds$prnp_hit & !(compounds$pubchem_cid %in% luciferase_inhibitors$pubchem_cid[luciferase_inhibitors$pubchem_activity_outcome=='Active'])
+hit_table = rbind(hit_table,cbind('Not luciferase inhibitors',sum(compounds$prnp_hit)))
+compounds$prnp_hit = compounds$prnp_hit & !(compounds$pubchem_cid %in% prnp_dose_1$pubchem_cid[prnp_dose_1$pubchem_activity_outcome=='Inactive'])
+compounds$prnp_hit = compounds$prnp_hit & !(compounds$pubchem_cid %in% prnp_dose_2$pubchem_cid[prnp_dose_2$pubchem_activity_outcome=='Inactive'])
+hit_table = rbind(hit_table,cbind('Not found inactive in PRNP dose-response',sum(compounds$prnp_hit)))
+compounds$prnp_hit = compounds$prnp_hit & !(compounds$pubchem_cid %in% prnp_cherrypick$pubchem_cid[prnp_cherrypick$pubchem_activity_outcome=='Inactive'])
+hit_table = rbind(hit_table,cbind('Not found inactive in cherry pick re-testing',sum(compounds$prnp_hit)))
+compounds$prnp_hit = compounds$prnp_hit & compounds$fehta_z > 2 & !is.na(compounds$fehta_z)
+hit_table = rbind(hit_table,cbind('Also active in Scripps FRET screen',sum(compounds$prnp_hit)))
 
 hit_table
+compounds$smiles[compounds$prnp_hit]
+
+compounds$prnp_nominal = compounds$pubchem_cid %in% prnp_5utr_primary$pubchem_cid[prnp_5utr_primary$pubchem_activity_outcome=='Active']
+compounds$fehta_nominal = compounds$pubchem_cid %in% fehta_primary$pubchem_cid[fehta_primary$pubchem_activity_outcome=='Active']
+sum(compounds$fehta_nominal)
+
 
 # final hit rate
-sum(prnp$hit) / nrow(prnp)
+mean(compounds$prnp_hit[!is.na(compounds$prnp_z)])
 
 # generate ECFP6 fingerprints for each compound
 fps = list(nrow(cid_smiles))
@@ -137,18 +152,14 @@ for (i in 1:nrow(cid_smiles)) {
   fps[i] = fp
 }
 
-# to save compute time:
-# cluster only the hits, and hits vs. non-hits, but don't cluster the non-hits with each other
-cid_smiles$screened = cid_smiles$pubchem_cid %in% prnp$pubchem_cid
-cid_smiles$hit = cid_smiles$pubchem_cid %in% prnp$pubchem_cid[prnp$hit]
-hit_fps = fps[cid_smiles$hit]
-screened_fps = fps[cid_smiles$screened]
-screened_fps_test = screened_fps[1:1000]
+hit_fps = fps[compounds$prnp_hit]
+screened_fps = fps[!is.na(compounds$prnp_z)]
 
-hit_vs_all_sim = fp.sim.matrix(fplist=hit_fps[1:10], fplist2=screened_fps_test, method='tanimoto')
-hit_vs_all_dist = 1 - hit_vs_all_sim
-haclust = hclust(as.dist(hit_vs_all_dist))
-plot(haclust)
+# screened_fps_test = screened_fps[1:1000]
+# hit_vs_all_sim = fp.sim.matrix(fplist=hit_fps[1:10], fplist2=screened_fps_test, method='tanimoto')
+# hit_vs_all_dist = 1 - hit_vs_all_sim
+# haclust = hclust(as.dist(hit_vs_all_dist))
+# plot(haclust)
 
 hit_dist = 1 - fp.sim.matrix(hit_fps, method='tanimoto')
 clustering = hclust(as.dist(hit_dist))
